228d6b7b4563e0c60eb2650fea4715be0ef58b85
andiedoescode <pang.andrea@gmail.com>
2025-08-27 15:37:42 -0400

    Added --verbose flag, initialize list of messages

--- main.py
+++ main.py
@@ -1,28 +1,45 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
-import sys
+from google.genai import types
 
 def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
-
     gen_model = "gemini-2.0-flash-001"
-    user_args = sys.argv[1:]
 
+    # Check for --verbose flag
+    verbose = "--verbose" in sys.argv
+    user_args = []
+    for arg in sys.argv[1:]:
+        if not arg.startswith("--"):
+            user_args.append(arg)
+
+    # Error message and exit if script run without input
     if not user_args:
         print('Prompt error.')
-        print('Syntax: python main.py "Your prompt here"')
+        print('Syntax: python main.py "Your prompt here" [--verbose]')
         print('Example: python main.py "How do I build a calculator app?"')
         sys.exit(1)
 
+    # Store user prompt and initialize list of messages
     user_prompt = " ".join(user_args)
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)])
+    ]
 
-    response = client.models.generate_content(model=gen_model, contents=user_prompt)
+    response = client.models.generate_content(model=gen_model, contents=messages)
 
-    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-    print("Response tokens:", response.usage_metadata.candidates_token_count)
+    # Extra info if --verbose flag used
+    if verbose:
+        print("User prompt:", user_prompt)
+        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+        print("Response tokens:", response.usage_metadata.candidates_token_count)
+    
+    # Print LLM response
+    print("Response:")
     print(response.text)
 
 
